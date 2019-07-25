4e1d467f2113344bb9c5bec4ee3dc518f3022632
Miroslav Vasilev <miroslav.vasilev@scalefocus.com>
2019-07-25 16:48:14 +0300

    Initial Commit

---
+++ OdeToFood.Data/IRestaurantRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantRepository
+    {
+        IEnumerable<Restaurant> GetAll();
+
+        IEnumerable<Restaurant> GetRestaurantsByName(string name);
+    }
+}
---
+++ OdeToFood.Data/InMemoryRestaurantRepository.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    public class InMemoryRestaurantRepository : IRestaurantRepository
+    {
+        private List<Restaurant> restaurants;
+
+        public InMemoryRestaurantRepository()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant(){ Id = 0, Name = "Burger Place", Cuisine = CuisineType.None, Location = "New York"},
+                new Restaurant(){ Id = 1, Name = "Lou's Pizzeria", Cuisine = CuisineType.Italian, Location = "Burgas"},
+                new Restaurant(){ Id = 2, Name = "Kebab World", Cuisine = CuisineType.Indian, Location = "Whatever"}
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name);
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name = "")
+        {
+            return string.IsNullOrEmpty(name) ? GetAll() : restaurants.FindAll(restaurant => restaurant.Name.StartsWith(name));
+        }
+    }
+}
